1fc48642986815806e7d4284effb633fe767e4f4
Xu <erpeng.xu@hp.com>
2019-06-05 17:23:49 +0800

    hello

---
+++ src/main/java/org/joseph/hello/controller/DemoController.java
@@ -0,0 +1,14 @@
+package org.joseph.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+	
+}
