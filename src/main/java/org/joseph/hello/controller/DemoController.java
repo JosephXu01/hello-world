488e0edb5f66f18e376aafb50b262f642e7e6ca6
joseph <xumotu@163.com>
2019-06-21 22:51:52 +0800

    redis

--- src/main/java/org/joseph/hello/controller/DemoController.java
+++ src/main/java/org/joseph/hello/controller/DemoController.java
@@ -1,15 +1,29 @@
 package org.joseph.hello.controller;
 
+import org.joseph.hello.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class DemoController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 	
 	@GetMapping("/")
 	public String home() {
 		System.out.println("Log - Hello World");
 		return "Hello World";
+	}
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
 	}
 	
 }
